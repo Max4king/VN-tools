fecbd8f62caec072eee711ee055c1220dd8ee41b
Max4king <88973277+Max4king@users.noreply.github.com>
2023-04-04 18:22:22 +0200

    fixing the script
    Script does not work correctly. This should fix it. For more detail. See #1

--- vnSelectorLE.bat
+++ vnSelectorLE.bat
@@ -3,10 +3,12 @@ setlocal enabledelayedexpansion
 
 rem %~dp0 for current directory that this batch is at
 rem Change it to your own directory where you store your VN shortcut
+rem ex: D:\Games\VN Selection\
 set "folder=%~dp0"
+
 rem Change this is Your own location. This is the Default One
 rem C:\Program Files\Locale.Emulator.2.5.0.1\LEProc.exe
-set "LE=C:\Program Files\Locale.Emulator.2.5.0.1\LEProc.exe"
+set "LE=D:\Locale.Emulator.2.5.0.1\LEProc.exe"
 rem Get list of .lnk files in current directory
 set "count=0"
 for %%f in ("%folder%*.lnk") do (
@@ -23,11 +25,13 @@ for /l %%i in (1,1,%count%) do (
 set /p choice=Enter file number:
 
 rem Run selected file
-set "name=!file[%choice%]!.lnk"
+set "name=!folder!!file[%choice%]!.lnk"
+echo "!name!"
 if exist "!name!" (
 	for /f "usebackq delims=" %%P in (`powershell.exe -c "(New-Object -ComObject WScript.Shell).CreateShortcut('%name%').TargetPath"`) do set "name=%%P"
 ) else (
     echo Invalid choice
+	pause
     exit /b 1
 )
 rem Change the PATH to your own directory
@@ -35,5 +39,4 @@ echo "!name!"
 
 start "" "!LE!" "!name!" && start cmd /c "echo Enjoy Your VN :D && echo This should close automatically If not then close it yourself && timeout /t 3 > nul" && exit
 
-rem extra exit just incase.
 exit
