b0a787d6acef89f178eed882afa0456f0e69a982
Max4king <ryan3311@hotmail.com>
2023-10-18 17:06:30 +0200

    fix folder name launcher

--- runVN.bat
+++ runVN.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "python_script_path=C:\Games\VN\VN-quick-start\VNSelLE.py"
+set "python_script_path=C:\Games\VN\VN-tools\VNSelLE.py"
 
 rem Check if Python script exists
 if not exist "%python_script_path%" (
